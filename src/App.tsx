52c4369a9a36ede0020c43385536ee82b971997a
Anil Prajapat <prajapatanil200@gmail.com>
2025-03-05 00:00:30 +0530

    remove query devtool

--- src/App.tsx
+++ src/App.tsx
@@ -1,12 +1,12 @@
 import { QueryClient, QueryClientProvider } from "@tanstack/react-query";
-import { ReactQueryDevtools } from "@tanstack/react-query-devtools";
 import { BrowserRouter, Route, Routes } from "react-router-dom";
 import { ThemeProvider } from "./context/theme-provider";
 import WeatherDashboard from "./pages/WeatherDashboard";
 import CityPage from "./pages/CityPage";
 import Layout from "./components/layout";
 import { Toaster } from "sonner";
- function App () {
+function App() {
+
   const queryClient = new QueryClient({
     defaultOptions: {
       queries: {
@@ -32,8 +32,7 @@ import { Toaster } from "sonner";
             <Toaster richColors />
           </ThemeProvider>
         </BrowserRouter>
-        <ReactQueryDevtools initialIsOpen={false} />
-      </QueryClientProvider>
+           </QueryClientProvider>
     </>
   );
 }
